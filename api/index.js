a8d9c5b647bb553585dcce26cd8ba5f0c642a348
chanchal chourasiya <chanchalchourasiya983@gmail.com>
2026-01-26 21:48:42 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -1,1 +1,26 @@
-export { default } from '../dist/server/index.js';
+import * as server from "../dist/server/index.js";
+
+export default async function handler(req, context) {
+  const url = new URL(
+    req.url || "/",
+    `https://${req.headers.host}`
+  );
+
+  const request = new Request(url.toString(), {
+    method: req.method,
+    headers: req.headers,
+    body: req.body
+  });
+
+  const env = {
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    PUBLIC_STOREFRONT_API_TOKEN: process.env.PUBLIC_STOREFRONT_API_TOKEN,
+    PUBLIC_STORE_DOMAIN: process.env.PUBLIC_STORE_DOMAIN
+  };
+
+  const executionContext = {
+    waitUntil: context.waitUntil || (() => {})
+  };
+
+  return server.fetch(request, env, executionContext);
+}
