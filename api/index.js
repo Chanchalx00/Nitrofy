27f3b0816a79f613f5f3fc83877cb5b4cd1fa4ef
Chanchal chourasia <132729182+Chanchalx00@users.noreply.github.com>
2026-01-30 16:38:04 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
+export const runtime = 'edge';
+
 import server from '../dist/server/index.js';
 
 export default {
-  async fetch(request, env, executionContext) {
+  fetch(request, env, executionContext) {
     return server.fetch(request, env, executionContext);
   },
 };
