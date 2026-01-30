4756130217db1c34110b748adf5732d549d1c552
chanchal chourasiya <chanchalchourasiya983@gmail.com>
2026-01-30 23:55:39 +0530

    fix

--- react-router.config.js
+++ react-router.config.js
@@ -1,7 +1,19 @@
 export default {
   appDirectory: 'app',
   buildDirectory: 'dist',
-  ssr: true,
+  watchPaths: ['./public', './.env'],
+  server: './server.js',
+  publicPath: (process.env.HYDROGEN_ASSET_BASE_URL ?? '/') + 'build/',
+  assetsBuildDirectory: 'dist/client/assets',
+  serverConditions: ['worker', process.env.NODE_ENV],
+  serverDependenciesToBundle: 'all',
+  serverModuleFormat: 'esm',
+  serverPlatform: 'neutral',
+  serverMinify: process.env.NODE_ENV === 'production',
+  tailwind: true,
+  postcss: true,
+  serverBuildPath: 'dist/server/index.js',
+  serverMainFields: ['browser', 'module', 'main'],
 };
 
 /** @typedef {import('@react-router/dev/config').Config} Config */
